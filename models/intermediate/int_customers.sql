635eb545c80328eed6b523d7057b8f6d163118d6
Kamesh V <hari8754261@gmail.com>
2026-05-18 06:50:03 +0000

    Created intermediate models

--- models/intermediate/int_customers.sql
+++ models/intermediate/int_customers.sql
@@ -1,16 +1,39 @@
-with combined as (
+with c as (
     select 
-        c.customer_id,
-        c.name as customer_name, 
-        c.address,
-        c.phone_number,
-        c.account_balance,
-        c.market_segment,
-        n.name as nation_name,
-        n.comment
-    from {{ ref('stg_customers') }} c 
-    join {{ ref('stg_nations') }} n
-        on c.nation_id = n.nation_id 
+        customer_id,
+        name as customer_name,
+        address,
+        phone_number,
+        account_balance,
+        market_segment,
+        nation_id,
+        comment
+    from {{ ref('stg_customers')}}
+    -- where customer_id <= 30010
+),
+
+n as (
+    select 
+        nation_id,
+        region_id,
+        name as nation_name
+    from {{ ref('stg_nations')}}
+),
+
+r as (
+    select 
+        region_id,
+        name as region_name
+    from {{ ref('stg_regions') }}
 )
 
-select * from combined
+select 
+    c.* exclude(nation_id, comment),
+    n.nation_id,
+    r.region_name,
+    c.comment
+from c
+join n 
+    on c.nation_id = n.nation_id
+join r 
+    on n.region_id = r.region_id
