bfef72dc96585729de38ab31c6bfced9d36aee7c
Kamesh V <hari8754261@gmail.com>
2026-05-19 05:53:22 +0000

    created some test

--- models/intermediate/int_customers.sql
+++ models/intermediate/int_customers.sql
@@ -1,4 +1,5 @@
 with c as (
+
     select 
         customer_id,
         name as customer_name,
@@ -8,32 +9,39 @@ with c as (
         market_segment,
         nation_id,
         comment
-    from {{ ref('stg_customers')}}
-    -- where customer_id <= 30010
+    from {{ ref('stg_customers') }}
+
 ),
 
 n as (
+
     select 
         nation_id,
-        region_id,
-        name as nation_name
-    from {{ ref('stg_nations')}}
+        name as nation_name,
+        region_id
+    from {{ ref('stg_nations') }}
+
 ),
 
 r as (
+
     select 
         region_id,
         name as region_name
     from {{ ref('stg_regions') }}
+
 )
 
 select 
     c.* exclude(nation_id, comment),
-    n.nation_id,
+    n.nation_name,
     r.region_name,
     c.comment
+
 from c
+
 join n 
     on c.nation_id = n.nation_id
+
 join r 
     on n.region_id = r.region_id
